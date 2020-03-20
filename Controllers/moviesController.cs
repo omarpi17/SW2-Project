5481a81d8d75dad05ebf214ac3f9c1d3529ca87d
omarpi17 <62350039+omarpi17@users.noreply.github.com>
2020-03-20 18:08:30 +0200

    blogger
    once the users registered they can post,like and comment on timeline
    also they can follow each others,share their experiences

---
+++ Controllers/moviesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "meg" };
+            return View(movie);
+        }
+    }
+}
